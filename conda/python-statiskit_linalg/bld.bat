4b3f1a92e7acbc633c77cc7f5daf752bf173fd48
Pierre Fernique <pfernique@gmail.com>
2017-01-30 10:25:13 +0100

    Update python-statiskit_linalg

--- conda/python-statiskit_linalg/bld.bat
+++ conda/python-statiskit_linalg/bld.bat
@@ -2,8 +2,8 @@ echo ON
 
 set SCONS_MSCOMMON_DEBUG=-
 scons py --prefix=%LIBRARY_PREFIX% -j%CPU_COUNT% --arch=%ARCH%
-if errorlevel 1 exit 1
+if errorlevel neq 0 exit 1
 python setup.py install
-if errorlevel 1 exit 1
+if errorlevel neq 0 exit 1
 
 echo OFF
