51bcd9a54ec74f9e7a3fdacc2b8fa4c426608162
Pierre Fernique <pfernique@gmail.com>
2017-11-07 08:23:02 +0100

    Update Eigen.h

--- src/cpp/Eigen.h
+++ src/cpp/Eigen.h
@@ -3,8 +3,8 @@
 
 #include <stdexcept>
 #include <vector>
-#include <eigen3/Eigen/Dense>
-#include <eigen3/Eigen/Sparse>
+#include <Eigen/Dense>
+#include <Eigen/Sparse>
 
 #if defined WIN32 || defined _WIN32 || defined __CYGWIN__
     #ifdef LIBSTATISKIT_LINALG
