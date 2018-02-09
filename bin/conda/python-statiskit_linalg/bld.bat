51d1313b44a67d262f88848746b8c225d00c2115
Pierre Fernique <pfernique@gmail.com>
2018-02-09 12:32:21 +0100

    Update recipes

--- bin/conda/python-statiskit_linalg/bld.bat
+++ bin/conda/python-statiskit_linalg/bld.bat
@@ -1,5 +1,10 @@
 echo ON
 
+if "%PY3K%" == "1" (
+  2to3 -n -w %SRC_DIR%\src\py\autowig
+  2to3 -n -w %SRC_DIR%\test
+)
+
 scons py --prefix=%LIBRARY_PREFIX% -j%CPU_COUNT% --arch=%ARCH% --test-level=none
 if errorlevel 1 exit 1
 
--- bin/conda/python-statiskit_linalg/build.sh
+++ bin/conda/python-statiskit_linalg/build.sh
@@ -1,5 +1,10 @@
 set -ve
 
+if [[ "$PY3K" = "1" ]]; then
+  2to3 -n -w $SRC_DIR/src/py/statiskit
+  2to3 -n -w $SRC_DIR/test
+fi
+
 scons py --prefix=$PREFIX -j$CPU_COUNT
 
 set +ve
